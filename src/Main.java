1397170d0e47d487ca6c29d9a7de9d1e060da66d
Nisflei <ncgaloni@hotmail.com>
2023-05-17 14:38:30 -0300

    versao2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Revisão Germinare TECH - E!");
+        System.out.println("informe seu perfil: ");
+        String texto = sc.nextLine();
+
     }
 }
